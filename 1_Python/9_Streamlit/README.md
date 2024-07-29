8746acaf778629bd86bfbdc59f0d1222b3c7495e
Arpit Dubey <aarpitdubey@gmail.com>
2024-07-29 22:46:59 +0530

    Learning Streamlit

--- 1_Python/9_Streamlit/README.md
+++ 1_Python/9_Streamlit/README.md
@@ -17,3 +17,19 @@ st.title("Title you want to show")
 ```
 
 ![img](./img/streamlit.gif "Author: Arpit Dubey")
+
+Lets add a Text "Successfully Login!!"
+
+```python
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Title of the application
+st.title('Welcome Arpit Dubey!')
+
+# Display a Simple Text
+st.write('Successfully Login!!')
+```
+
+![img](./img/streamlit1.gif "Author: Arpit Dubey")
--- 1_Python/9_Streamlit/app.py
+++ 1_Python/9_Streamlit/app.py
@@ -3,5 +3,19 @@ import pandas as pd
 import numpy as np
 
 # Title of the application
-
 st.title('Welcome Arpit Dubey!')
+
+# Display a Simple Text
+st.write('Successfully Login!!')
+
+# Creating a Simple dataframe
+
+df = pd.DataFrame({
+    'Height': [162, 164, 170, 180],
+    'Experience': [4, 7, 5, 8],
+    'Salary': [60000, 180000, 55000, 90000]
+})
+
+# Display Dataframe
+st.write('This is the simple dataframe :')
+st.write(df)
